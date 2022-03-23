9082611797f10e4a52d32678bde2fe7d2d5c1778
Aviva J J <98533789+afried55@users.noreply.github.com>
2022-03-23 23:42:26 +0000

    lab

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+This program finds the number of iterations it takes a number to get to one with the 3n+1 function and then graphs it. 
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+None
 
 ## REFERENCES:
- < List any outside resources used >
+None
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+None
--- main.py
+++ main.py
@@ -1,18 +1,57 @@
-
+import turtle
 def seq3np1(n):
     """
         Print the 3n+1 sequence from n, terminating when it reaches 1.
         args: n (int) starting value for 3n+1 sequence
-        return: None
+        return: count
     """
+    count=0
     while(n != 1):
-        print(n)
         if(n % 2) == 0:        # n is even
             n = n // 2
+            count=count+1
         else:                 # n is odd
             n = n * 3 + 1
-    print(n)                  # the last print is 1
-
+            count=count+1
+                      # the last print is 1
+    return count
+def draw_graph(bound=None):
+  ''' This function draws a graph of the 3n+1 sequence
+  args: bound (int)
+  returns: None
+  '''
+  wn = turtle.Screen()
+  drawer = turtle.Turtle()
+  writer = turtle.Turtle()
+  wn.setworldcoordinates(0,0,10,10)
+  max_so_far = 0
+  label = None
+  drawer.goto(1,0)
+  for i in range(1,bound+1):
+    result = seq3np1(i)
+    if result > max_so_far:
+      max_so_far = result
+      label = 'Maximum so far: '+ str(i) + ", " + str(result)
+      writer.clear()
+    writer.up()
+    writer.goto(0,max_so_far)
+    writer.write(label,align = "left")
+    wn.setworldcoordinates(0,0,i+10,max_so_far+10)
+    drawer.goto(i, result)
+  wn.exitonclick()
+  
+  
+  
 def main():
-	seq3np1(3)
+  print(seq3np1(3))
+  upper_bound = int(input("What is the upper bound?"))
+  if upper_bound < 1:
+    return
+  start = upper_bound+1
+  for i in range(1,start):
+    iterations = seq3np1(i)
+    print("Current loop value: ",i, " Iterations: ", iterations)
+  draw_graph(upper_bound)
+
 main()
+
